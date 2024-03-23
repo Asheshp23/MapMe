b53f27e1d774d4ba82b3f49f47cf460d10aacb9a
asheshp23 <asheshp23@gmail.com>
2024-03-22 20:03:12 -0400

    WIP map me feature

---
+++ MapMe/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  MapMe
+//
+//  Created by Ashesh Patel on 2024-03-19.
+//
+
+import Foundation
---
+++ MapMe/MapMeView.swift
@@ -0,0 +1,18 @@
+//
+//  MapMeView.swift
+//  MapMe
+//
+//  Created by Ashesh Patel on 2024-03-19.
+//
+
+import SwiftUI
+
+struct MapMeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapMeView()
+}
