7bd9d1fa172afd53fa80d407a583924b01d60e07
asheshp23 <asheshp23@gmail.com>
2024-03-31 10:17:28 -0400

    Updated location manager

--- MapMe/LocationManager.swift
+++ MapMe/LocationManager.swift
@@ -1,47 +1,52 @@
-import Foundation
-import UIKit
+import SwiftUI
 import CoreLocation
 
 @Observable
 class LocationManager: NSObject, CLLocationManagerDelegate {
-  private let locationManager = CLLocationManager()
-  
+  @ObservationIgnored let manager = CLLocationManager()
   var userHeading: CLLocationDirection = 0.0
   var userLocation: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 0, longitude: 0)
-  
   var userLocations: [CLLocationCoordinate2D] = []
+  var isAuthorized = false
   
   override init() {
-    locationManager.desiredAccuracy = kCLLocationAccuracyBest
-    locationManager.requestAlwaysAuthorization()
-    locationManager.startUpdatingLocation()
-    locationManager.startUpdatingHeading()
-   
     super.init()
-    updateHeadingOrientation()
-    locationManager.delegate = self
+    manager.delegate = self
+    startLocationServices()
   }
   
-  func updateHeadingOrientation() {
+  func headingOrientation() -> CLDeviceOrientation {
     let deviceOrientation = UIDevice.current.orientation
     let clDeviceOrientation: CLDeviceOrientation
     
     switch deviceOrientation {
     case .portrait:
-      clDeviceOrientation = .portrait
+      return .portrait
     case .portraitUpsideDown:
-      clDeviceOrientation = .portraitUpsideDown
+      return .portraitUpsideDown
     case .landscapeLeft:
-      clDeviceOrientation = .landscapeRight // Note the flipped mapping
+      return .landscapeRight // Note the flipped mapping
     case .landscapeRight:
-      clDeviceOrientation = .landscapeLeft // Note the flipped mapping
+      return .landscapeLeft // Note the flipped mapping
     case .faceUp, .faceDown, .unknown:
       fallthrough
     default:
-      clDeviceOrientation = .unknown // Default or unchanged
+      return .unknown
     }
-    
-    locationManager.headingOrientation = clDeviceOrientation
+  }
+  
+  func startLocationServices() {
+    if manager.authorizationStatus == .authorizedAlways || manager.authorizationStatus == .authorizedWhenInUse {
+      manager.desiredAccuracy = kCLLocationAccuracyBest
+      manager.allowsBackgroundLocationUpdates = true
+      manager.headingOrientation = headingOrientation()
+      manager.startUpdatingLocation()
+      manager.stopUpdatingHeading()
+      isAuthorized = true
+    } else {
+      isAuthorized = false
+      manager.requestWhenInUseAuthorization()
+    }
   }
   
   func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -58,7 +63,24 @@ class LocationManager: NSObject, CLLocationManagerDelegate {
     userHeading = heading
   }
   
+  func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+    switch manager.authorizationStatus {
+    case .authorizedAlways, .authorizedWhenInUse:
+      isAuthorized = true
+      manager.requestLocation()
+    case .notDetermined:
+      isAuthorized = false
+      manager.requestWhenInUseAuthorization()
+    case .denied:
+      isAuthorized = false
+      print("access denied")
+    default:
+      isAuthorized = true
+      startLocationServices()
+    }
+  }
+  
   func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-    print("Error getting location: \(error)")
+    print(error.localizedDescription)
   }
 }
