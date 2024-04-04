24429b79977ec3a48196f15355927e1cae77a5e2
asheshp23 <asheshp23@gmail.com>
2024-04-04 00:08:32 -0400

    Fixed custom user location heading issue

--- MapMe/MapMeView.swift
+++ MapMe/MapMeView.swift
@@ -4,13 +4,15 @@ import MapKit
 struct MapMeView: View {
   @State var locationManager = LocationManager()
   @State var cameraHeading: Double = 0.0
-
+  @Namespace var mapScope
+  @State var mapCameraPosition = MapCameraPosition.userLocation(followsHeading: true, fallback: .automatic)
+  
   var body: some View {
-    Map(interactionModes: [.pan, .zoom, .rotate]) {
-      Annotation("", coordinate: locationManager.userLocation) {
+    Map(position: $mapCameraPosition) {
+      Annotation("", coordinate: locationManager.userLocation, anchor: .center) {
         Image(systemName: "location.north.circle")
           .resizable()
-          .rotationEffect(.degrees( locationManager.userHeading - cameraHeading))
+          .rotationEffect(.degrees(locationManager.userHeading - cameraHeading))
           .foregroundStyle(.blue)
           .frame(maxWidth: 80, maxHeight: 80)
           .background(
@@ -18,11 +20,18 @@ struct MapMeView: View {
               .fill(.white)
           )
       }
-      
       MapPolyline(coordinates: locationManager.userLocations)
         .stroke(.teal, lineWidth: 2.0)
       
     }
+    .mapScope(mapScope)
+    .mapControls {
+      MapPitchToggle(scope: mapScope)
+      MapUserLocationButton(scope: mapScope)
+      MapCompass(scope: mapScope)
+      MapScaleView(scope: mapScope)
+    }
+    .mapControlVisibility(.visible)
     .mapStyle(.hybrid(elevation: .realistic))
     .onMapCameraChange { mapCameraUpdateContext in
       Task { @MainActor in
