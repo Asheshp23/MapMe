7c9240dd42002ac9a1dbc2d3a2db8e2fdac31924
asheshp23 <asheshp23@gmail.com>
2024-04-03 23:42:20 -0400

    Fixed user location heading issue

--- MapMe/MapMeView.swift
+++ MapMe/MapMeView.swift
@@ -3,13 +3,14 @@ import MapKit
 
 struct MapMeView: View {
   @State var locationManager = LocationManager()
+  @State var cameraHeading: Double = 0.0
 
   var body: some View {
-    Map() {
+    Map(interactionModes: [.pan, .zoom, .rotate]) {
       Annotation("", coordinate: locationManager.userLocation) {
         Image(systemName: "location.north.circle")
           .resizable()
-          .rotationEffect(.degrees(locationManager.userHeading))
+          .rotationEffect(.degrees( locationManager.userHeading - cameraHeading))
           .foregroundStyle(.blue)
           .frame(maxWidth: 80, maxHeight: 80)
           .background(
@@ -20,10 +21,16 @@ struct MapMeView: View {
       
       MapPolyline(coordinates: locationManager.userLocations)
         .stroke(.teal, lineWidth: 2.0)
-        
+      
     }
-    .mapStyle(.imagery)
+    .mapStyle(.hybrid(elevation: .realistic))
+    .onMapCameraChange { mapCameraUpdateContext in
+      Task { @MainActor in
+        cameraHeading = mapCameraUpdateContext.camera.heading
+      }
+    }
   }
+  
 }
 
 #Preview {
